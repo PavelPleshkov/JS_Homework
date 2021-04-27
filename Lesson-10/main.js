0aa657d9d7276f271c3174e554851b3e6a52cb1f
Pavel Pleshkov <pleshkov.p@gmail.com>
2021-04-27 16:23:00 +0300

    Homework 10. Task 2

--- Lesson-10/main.js
+++ Lesson-10/main.js
@@ -1,3 +1,4 @@
+// task1
 function Animal(name) {
     this.name = name;
     this.foodAmount = 50;
@@ -64,3 +65,47 @@ barsik.stroke();
 barsik.feed().stroke().feed().stroke();
 
 barsik = null;
+
+//task2
+function deepClone(obj) {
+    var clonedObj = (Array.isArray(obj)) ? [] : {};
+
+    for (var key in obj) {
+        clonedObj[key] = (typeof obj[key] == 'object' && obj[key]) ? deepClone(obj[key]) : obj[key];
+    }
+
+    return clonedObj;
+}
+
+var initialObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+var clonedObj = deepClone(initialObj);
+
+clonedObj.object.object2.array2[1].name = 'Vasya';
+clonedObj.array.push(2);
+
+console.log(initialObj);
+console.log(clonedObj);
+
+
+//task3
+function compareObjects(obj1, obj2) {
+
+}
