9eddaea9b8f64b95fc2ac307dd50a95d8f2d384e
Pavel Pleshkov <pleshkov.p@gmail.com>
2021-05-11 17:58:54 +0300

    Homework 13

--- Lesson-13/main.js
+++ Lesson-13/main.js
@@ -1,13 +1,48 @@
-var btn = document.getElementById('data-tdBtn');
-var table = document.getElementById('data-table');
-var tableBody = document.getElementById('data-tableBody')
-var row = tableBody.firstElementChild;
+var btn = document.getElementById('addBtn');
+var tableBody = document.getElementById('tableBody')
+var row = tableBody.firstElementChild.cloneNode(true);
 
 btn.addEventListener('click', addRow);
 
 function addRow(e) {
-    console.log('btn pressed');
     // console.dir(row);
-    tableBody.insertBefore(row.cloneNode(true), tableBody.firstElementChild);
-    // tableBody.appendChild(row.cloneNode(true));
+    var newRow = row.cloneNode(true);
+
+    tableBody.insertBefore(newRow, tableBody.firstElementChild);
+    console.log('new row added', newRow);
+}
+
+tableBody.addEventListener('click', activateTd);
+
+function activateTd(e) {
+
+    if (e.target.tagName == 'TD') {
+        var input = document.createElement('input');
+        input.type = 'text';
+        input.addEventListener('blur', diactivateTd);
+        input.addEventListener('keydown', diactivateTdByEnter);
+
+        function diactivateTd() {
+            e.target.innerHTML = input.value;
+        }
+
+        function diactivateTdByEnter(event) {
+            if (event.keyCode == 13) {
+                e.target.innerHTML = input.value;
+            }
+        }
+
+        if (!e.target.innerHTML) {
+            e.target.insertAdjacentElement('afterbegin', input);
+            input.focus();
+        } else if (!e.target.firstElementChild) {
+            e.target.innerHTML = '<input type="text" value ="' + e.target.innerHTML + '">';
+            input = e.target.firstElementChild;
+            input.focus();
+            input.addEventListener('blur', diactivateTd);
+            input.addEventListener('keydown', diactivateTdByEnter);
+        } else if (e.target.firstElementChild.tagName == 'INPUT') {
+            e.target.firstElementChild.focus();
+        }
+    }
 }
