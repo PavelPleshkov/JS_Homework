c6abec88735fd05c0a7696ad48b93fae5f0f933c
Pavel Pleshkov <pleshkov.p@gmail.com>
2021-05-21 08:53:28 +0300

    HW16. add first user link active

--- Lesson-16/scripts/main.js
+++ Lesson-16/scripts/main.js
@@ -1,22 +1,87 @@
 var container = document.getElementById('container');
 var main = document.getElementsByClassName('main')[0];
 var btn = document.getElementById('getBtn');
+var users = document.getElementsByClassName('users')[0];
 
 btn.addEventListener('click', getData);
 
 function getData() {
     var request = new XMLHttpRequest();
 
-    request.open('GET', 'https://reqres.in/api1/users?page=2', true);
+    request.open('GET', 'https://reqres.in/api/users?page=2', true);
     request.send();
 
     request.onload = function () {
         var statusType = Math.round(this.status / 100);
 
         if (statusType === 2) {
+            var data = JSON.parse(this.response).data;
             console.log(JSON.parse(this.response).data);
 
+            var usersList = document.createElement('div');
+            usersList.classList.add('usersList');
+            
+            if (users.children[0]) {
+                users.replaceChild(usersList, users.children[0]);
+            } else {
+                users.appendChild(usersList);
+            }
+            
 
+            usersList.addEventListener('click', activateUser);
+            var activeLink;
+            function activateUser(e) {
+                if (e.target.classList.contains('userLink')) {
+                    e.preventDefault();
+                    if (activeLink) {
+                        activeLink.classList.remove('userLinkActive');
+                    }
+                    if (activeLink != usersList.children[0]) {
+                        usersList.children[0].classList.remove('userLinkActive');
+                    }
+                    activeLink = e.target;
+                    activeLink.classList.add('userLinkActive');
+                }
+
+            }
+
+
+            for (var i = 0; i < data.length; i++) {
+                var userLink = document.createElement('a');
+                userLink.setAttribute('href', '#');
+                userLink.setAttribute('id', data[i].id);
+                userLink.classList.add('userLink');
+                userLink = userLink.cloneNode(true);
+                userLink.innerHTML = 'User ' + (i + 1);
+                usersList.appendChild(userLink);
+            }
+            if (usersList.children[0]) {
+                usersList.children[0].classList.add('userLinkActive');
+            }
+            
+
+            var userData = document.createElement('div');
+
+            userData.classList.add('userData');
+            userData = userData.cloneNode(true);
+
+            if (users.children[1]) {
+                users.replaceChild(userData, users.children[1]);
+            } else {
+                users.appendChild(userData);
+            }
+
+            var userImg = document.createElement('img');
+
+            userImg.classList.add('userDataImg');
+            userImg = userImg.cloneNode(true);
+            // userData.appendChild(userImg);
+
+            for (i = 0; i < data.length; i++) {
+                // userImg.setAttribute('src', data[i].avatar);
+                console.log(data[i].avatar);
+            }
+            userData.appendChild(userImg);
 
         } else {
             console.log(this.status);
