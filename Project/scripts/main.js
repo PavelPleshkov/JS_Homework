6a1e701fd378502c44f9e804a863f628f7581f54
Pavel <pleshkov.p@gmail.com>
2021-06-12 18:13:59 +0300

    Project
    add start page

---
+++ Project/scripts/main.js
@@ -0,0 +1,63 @@
+const main = document.getElementsByClassName('main')[0];
+const mainBtns = document.getElementsByClassName('mainBtns')[0];
+const mainBtnWeight = mainBtns.getElementsByClassName('mainBtn_weight')[0];
+const mainBtnForce = mainBtns.getElementsByClassName('mainBtn_force')[0];
+const mainBtnBurn = mainBtns.getElementsByClassName('mainBtn_burn')[0];
+const infoBlock = document.createElement('div');
+infoBlock.classList.add('mainInfo');
+
+// mainBtnWeight.addEventListener('click', insertInfoText);
+// mainBtnForce.addEventListener('click', insertInfoText);
+// mainBtnBurn.addEventListener('click', insertInfoText);
+
+// function insertInfoText(e) {
+//     infoBlock.innerHTML = `You choose ${this.children[0].innerHTML}!`;
+    
+//     // this.classList.add('mainBtnActivated');
+// }
+
+
+mainBtns.addEventListener('click', addInfoBlock);
+mainBtns.addEventListener('click', fillInfoBlock);
+mainBtns.addEventListener('click', activateBtn);
+
+function addInfoBlock(e) {
+    if (e.target.tagName == 'BUTTON' || e.target.tagName == 'SPAN') {
+        if (main.lastElementChild.classList.contains('mainInfo')) {
+            main.replaceChild(infoBlock, main.lastElementChild)
+        } else {
+            main.appendChild(infoBlock);
+        }
+    }
+}
+
+function fillInfoBlock(e) {
+    let choise;
+
+    if (e.target.tagName == 'SPAN') {
+        choise = e.target.innerHTML;
+    } else if (e.target.tagName == 'BUTTON') {
+        choise = e.target.children[0].innerHTML;
+    } else {
+        return;
+    }
+
+    infoBlock.innerHTML = `You choose ${choise}!`;
+}
+
+let activeBtn;
+function activateBtn(e) {
+    if (e.target.classList.contains('mainBtn') || e.target.parentElement.classList.contains('mainBtn')) {
+        if (activeBtn) {
+            activeBtn.classList.remove('mainBtnActive');
+        }
+
+        if (e.target.classList.contains('mainBtn')) {
+            activeBtn = e.target;
+        } else {
+            activeBtn = e.target.parentElement;
+        }
+
+        activeBtn.classList.add('mainBtnActive');
+    }
+}
