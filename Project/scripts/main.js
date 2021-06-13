1f2ed49caee881ecfbafe1dfa70f5113c05f0a3b
Pavel <pleshkov.p@gmail.com>
2021-06-13 13:49:03 +0300

    Project add color themes

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -3,14 +3,19 @@ const mainBtns = document.getElementsByClassName('mainBtns')[0];
 const mainBtnWeight = mainBtns.getElementsByClassName('mainBtn_weight')[0];
 const mainBtnForce = mainBtns.getElementsByClassName('mainBtn_force')[0];
 const mainBtnBurn = mainBtns.getElementsByClassName('mainBtn_burn')[0];
-const infoBlock = document.createElement('div');
+const infoBlock = document.createElement('p');
 infoBlock.classList.add('mainInfo');
 let activeBtn;
+let way;
 
-window.onload = () => {
+// window.onload = () => {
+//     if (localStorage.getItem('way')) {
+//         way = localStorage.getItem('way');
+//         console.log(way);
+//     }
+// }
 
-}
-
+// mainBtns.addEventListener('click', saveWayToLocalStorage);
 mainBtns.addEventListener('click', addInfoBlock);
 mainBtns.addEventListener('click', fillInfoBlock);
 mainBtns.addEventListener('click', activateBtn);
@@ -26,20 +31,22 @@ function addInfoBlock(e) {
 }
 
 function fillInfoBlock(e) {
-    let choise;
+    // if (!way) {
+        if (e.target.tagName == 'SPAN') {
+            way = e.target.innerHTML;
+        } else if (e.target.tagName == 'BUTTON') {
+            way = e.target.children[0].innerHTML;
+        } else {
+            return;
+        }
+    // }
+    
 
-    if (e.target.tagName == 'SPAN') {
-        choise = e.target.innerHTML;
-    } else if (e.target.tagName == 'BUTTON') {
-        choise = e.target.children[0].innerHTML;
-    } else {
-        return;
-    }
-
-    infoBlock.innerHTML = `You choose ${choise}!`;
+    infoBlock.innerHTML = `You choose ${way}!`;
+    
+    // return way;
 }
 
-// let activeBtn;
 function activateBtn(e) {
     if (e.target.classList.contains('mainBtn') || e.target.parentElement.classList.contains('mainBtn')) {
         if (activeBtn) {
@@ -55,3 +62,7 @@ function activateBtn(e) {
         activeBtn.classList.add('mainBtnActive');
     }
 }
+
+// function saveWayToLocalStorage(e) {
+//     localStorage.setItem('way', way);
+// }
