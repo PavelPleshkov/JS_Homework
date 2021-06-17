492a81beb5b48895802410ae351c0b5f54c487a7
Pavel <pleshkov.p@gmail.com>
2021-06-17 19:38:51 +0300

    HW22 finish

--- Lesson-22/main.js
+++ Lesson-22/main.js
@@ -143,9 +143,7 @@ function countVowelLetters(text) {
 function countVowelLetters(text) {
     const vowelLetters = ['а', 'я', 'ы', 'и', 'о', 'ё', 'у', 'ю', 'э', 'е', 'a', 'e', 'i', 'o', 'u', 'y'];
 
-    return [...text.toLowerCase()].filter( letter => {
-         return vowelLetters.includes(letter);
-    }).length;
+    return [...text.toLowerCase()].filter( letter => vowelLetters.includes(letter)).length;
 }
 
 countVowelLetters('Шла Саша по шоссе И сосала сУшку'); // 12
