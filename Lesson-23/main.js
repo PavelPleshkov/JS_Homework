922e802051b3c6e3da6ed98fe47edaa4e31bbeee
Pavel <pleshkov.p@gmail.com>
2021-06-17 19:13:43 +0300

    HW 23 start task4 classes

--- Lesson-23/main.js
+++ Lesson-23/main.js
@@ -113,6 +113,80 @@ function getUser(arr) {
 //  Задание 4:
 // Переписать последнее задание с ООП на новый синтаксис. Проверить работоспособность всех методов.
 
+function Animal(name) {
+    this.name = name;
+    this._foodAmount = 50;
+}
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+        this._foodAmount = 50;
+    }
+    
+}
+
+Animal.prototype._formatFoodAmount = function() {
+    return this._foodAmount + ' гр.';
+};
+
+Animal.prototype.dailyNorm = function(amount) {
+    if (!arguments.length) return this._formatFoodAmount();
+
+    if (amount < 50 || amount > 500) {
+        // throw new Error(amount + ' гр. - что-то не то в миске! :(');
+        return amount + ' гр. - что-то не то в миске! :(';
+    }
+
+    this._foodAmount = amount;
+};
+
+Animal.prototype.feed = function() {
+    console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+};
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.feed = function() {
+    Animal.prototype.feed.apply(this, arguments);
+    console.log('Кот доволен ^_^\n');
+
+    return this;
+};
+
+Cat.prototype.stroke = function() {
+    console.log('Гладим кота.\n');
+
+    return this;
+};
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.name);
+
+console.log(barsik.dailyNorm());
+// console.log(barsik.feed());
+barsik.feed();
+
+console.log(barsik.dailyNorm(600));
+// console.log(barsik.dailyNorm());
+// console.log(barsik.feed());
+barsik.feed();
+
+console.log(barsik.dailyNorm(250));
+// console.log(barsik.dailyNorm());
+// console.log(barsik.feed());
+barsik.feed();
+
+barsik.stroke();
+barsik.feed().stroke().feed().stroke();
+
+barsik = null;
 
 
 
